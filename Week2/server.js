ca0227f8525a14077d86e1f755d196b9cdb549d0
karathrop <karathrop@gmail.com>
2016-09-21 15:34:31 -0400

    updates to chat

--- Week2/server.js
+++ Week2/server.js
@@ -2,7 +2,7 @@ var http = require('http');
 var fs = require('fs'); // Using the filesystem module
 var httpServer = http.createServer(requestHandler);
 var url = require('url');
-httpServer.listen(8080);
+httpServer.listen(8081);
 
 function requestHandler(req, res) {
 
