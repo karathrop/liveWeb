1bd60dd9d62e285ee1817fff6b32500047706337
karathrop <karathrop@gmail.com>
2016-09-22 20:51:57 -0400

    updates

--- arthur/arthur.js
+++ arthur/arthur.js
@@ -4,8 +4,11 @@ function init() {
 
 				button.addEventListener("click", function() {
 					Page1.style.display="none";
+					Page3.style.display="none";
 					Page2.style.display="block";
-					introVid.style.display = "none";
+					birthdayvid.pause();
+					introVid.pause();
+					
 				})
 
 
@@ -19,11 +22,7 @@ function init() {
 				
 				
 				var lyric2 = document.getElementById("lyric2");
-				// var lyric2 = document.getElementById("lyric2");
-				// var lyric3 = document.getElementById("lyric3");
-				// var lyric4 = document.getElementById("lyric4");
-				// var lyric5 = document.getElementById("lyric5");
-				// var lyric6 = document.getElementById("lyric6");
+				
 
 				videos.onended = function() {
 					arthurDefault.style.display="block";
@@ -43,11 +42,14 @@ function init() {
 					videos.play();
 					arthurDefault.style.display="none";
 					arthur1yr.style.display="none";
+					
 					arthur3.style.display="none";
+					arthur4.style.display="none";
 					arthur5.style.display="none";
 					arthur6.style.display="none";
 					arthurDefault.style.display="none";
 					videos.style.display="block";
+					lyric1.style.color="white"
 					lyric2.style.color="white";
 					lyric3.style.color="white";
 					lyric4.style.color="white";
@@ -58,38 +60,53 @@ function init() {
 				}
 
 				lyric1.addEventListener("click", function()  {
-					arthur1yr.style.display = "block";
+					arthur1yr.style.display = "block";					
+					arthur3.style.display="none";
+					arthur5.style.display="none";
+					arthur6.style.display="none"
 					videos.style.display="none";
 					this.style.color="#cffde5";
 					
 				});
 				
 				lyric2.addEventListener("click", function()  {
+					console.log("video2")
 					playVideo("videos/arthur2.mp4");
+					arthur1yr.style.display="none";
 					arthur3.style.display="none";
+					arthur5.style.display="none";
+					arthur6.style.display="none"
+					//videos.style.display="none";
 					this.style.color="#cffde5";
 					
 				});
 				lyric3.addEventListener("click", function()  {
 					arthur3.style.display = "block";
+					arthur5.style.display="none";
+					arthur6.style.display="none";
+					arthur1yr.style.display = "none";
+					arthur4.style.display = "none";	
 					videos.style.display="none";
 					this.style.color="#cffde5";
 				});
-				// lyric3.addEventListener("click", function()  {
-				// 	playVideo("videos/vid3.mp4", "subtitles/vid3.vtt");
-				// 	this.style.color="#cffde5";
-				// });
-				// lyric4.addEventListener("click", function()  {
-				// 	playVideo("videos/vid4.mp4", "subtitles/vid4.vtt");
-				// 	this.style.color="#cffde5";
-				// });
+
+				lyric4.addEventListener("click", function()  {
+					arthur4.style.display = "block";
+					videos.style.display="none";
+					arthur1yr.style.display="none";
+					arthur3.style.display="none";
+					arthur5.style.display="none";
+					arthur6.style.display="none";					
+					this.style.color="#cffde5";
+				});
+				
 				lyric5.addEventListener("click", function()  {
 					arthur5.style.display = "block";
 					videos.style.display="none";
 					arthur1yr.style.display="none";
 					arthur3.style.display="none";
 					arthur6.style.display="none";
-					
+					arthur4.style.display="none";					
 					this.style.color="#cffde5";
 				});
 				lyric6.addEventListener("click", function()  {
@@ -97,14 +114,49 @@ function init() {
 					videos.style.display="none";
 					arthur1yr.style.display="none";
 					arthur3.style.display="none";
+					arthur4.style.display="none";
 					arthur5.style.display="none";
 					this.style.color="#cffde5";
 				});
 
-				backbutton = document.getElementById("backbutton");
+				lyric7.addEventListener("click", function()  {
+					playVideo("videos/arthur7.mp4");					
+					arthur1yr.style.display="none";
+					arthur3.style.display="none";
+					arthur5.style.display="none";
+					arthur4.style.display="none";
+					arthur6.style.display="none";
+					this.style.color="#cffde5";
+				});
+
+				var backbutton = document.getElementById("backbutton");
+				var backbuttonPage3 = document.getElementById("backbuttonPage3");
 
 				backbutton.addEventListener("click", function(){
+					Page3.style.display="none";
 					Page2.style.display="none";
-					Page1.style.display="block";				
-				})
+					Page1.style.display="block";
+					videos.pause();
+					birthdayvid.play();
+					introVid.play();
+				});
+				backbuttonPage3.addEventListener("click", function(){
+					Page3.style.display="none";
+					Page2.style.display="none";
+					Page1.style.display="block";
+					videos.pause();
+					birthdayvid.pause();
+					introVid.play();
+				});
+
+				songbutton = document.getElementById("songbutton");
+				songbutton.addEventListener("click", function(){
+					Page3.style.display="block";
+					Page1.style.display="none";
+					Page2.style.display="none";
+					introVid.pause();
+					videos.pause();
+					birthdayvid.play();
+				});
+
 		}
