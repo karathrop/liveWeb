e1fb2fc636eb67eeaa7cabc6fb77c25533fa4844
karathrop <karathrop@gmail.com>
2016-09-26 15:40:54 -0400

    new

--- Week3/httpserver.js
+++ Week3/httpserver.js
@@ -38,19 +38,32 @@ io.sockets.on('connection',
 	function (socket) {
 	
 		console.log("We have a new client: " + socket.id);
+	
+		// When this user "send" from clientside javascript, we get a "message"
+		// client side: socket.send("the message");  or socket.emit('message', "the message");
+		socket.on('message', 
+			// Run this function when a message is sent
+			function (data) {
+				console.log("message: " + data);
+				
+				// Call "broadcast" to send it to all clients (except sender), this is equal to
+				// socket.broadcast.emit('message', data);
+				socket.broadcast.send(data);
+				
+				// To all clients, on io.sockets instead
+				// io.sockets.emit('message', "this goes to everyone");
+			}
+		);
 		
 		// When this user emits, client side: socket.emit('otherevent',some data);
-		socket.on('othermouse', function(data) {
+		socket.on('otherevent', function(data) {
 			// Data comes in as whatever was sent, including objects
-			console.log("Received: 'othermouse' " + data.x + " " + data.y);
-			
-			// Send it to all of the clients
-			socket.broadcast.emit('othermouse', data);
+			console.log("Received: 'otherevent' " + data);
 		});
 		
 		
 		socket.on('disconnect', function() {
-			console.log("Client has disconnected " + socket.id);
+			console.log("Client has disconnected");
 		});
 	}
 );
